f91ebfe550dcc47d0a5c31105b2e76d58e99925a
Truong Trung Nghia <nghiatt4@fsoft.com.vn>
2017-12-15 10:16:43 +0900

    Add solution read_and_modify

---
+++ solutions/read_and_modify.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+pd.set_option('max.rows', 6)
+
+data = pd.read_csv(
+    'data/titanic.csv',
+    index_col='PassengerId',
+    usecols=lambda col: col not in ('Cabin', 'Ticket'),
+    dtype={
+        'Pclass': 'category',
+        'Sex': 'category',
+        'Embarked': 'category'
+    }
+)
