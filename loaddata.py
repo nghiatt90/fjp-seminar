0c8799252a023b7a2c0b834e49229e55076f651b
Truong Trung Nghia <nghiatt4@fsoft.com.vn>
2017-12-20 15:59:48 +0900

    Add feature engineer code to loaddata.py

--- loaddata.py
+++ loaddata.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import copy
+import re
+
 
 pd.set_option('max.rows', 14)
 
@@ -17,3 +20,19 @@ data['Age'] = (data['Age'].fillna(data['Age'].mean())
                           .astype(int))
 data['Embarked'].fillna(data['Embarked'].mode()[0], inplace=True)
 
+
+def get_title_from_name(name):
+    m = re.search(' ([A-Za-z]*)\.', name)
+    if not m:
+        raise ValueError(f'Name doesn\'t match pattern: {name}')
+    return m.group(1)
+
+
+data_engineered = data.copy(deep=True)
+data_engineered['Title'] = (data['Name'].apply(get_title_from_name)
+                                        .astype('category')
+                                        .cat.set_categories(('Mr', 'Mrs', 'Miss', 'Master', 'High Standing'))
+                                        .fillna('High Standing'))
+
+data_engineered['FamilySize'] = data_engineered['SibSp'] + data_engineered['Parch'] + 1
+data_engineered.drop(columns=['Name', 'SibSp', 'Parch'], inplace=True)
