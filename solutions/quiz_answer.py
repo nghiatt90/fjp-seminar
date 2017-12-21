404a3a3e7ad007ef3162222eb36e6c8899fcfb19
Truong Trung Nghia <nghiatt4@fsoft.com.vn>
2017-12-21 10:48:29 +0900

    Before going back and merge test set

---
+++ solutions/quiz_answer.py
@@ -0,0 +1,2 @@
+# Trong quá trình feature engineering, data visualization nên để data dưới dạng có ý nghĩa với con người
+# Chỉ chuyển về dạng số khi kết thúc phần việc của người và chuyển sang việc của máy
